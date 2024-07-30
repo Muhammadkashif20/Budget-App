3e05f3d8fb611510fcec59998871f05ab214b8f6
muhammadkashif20 <kashif28@gmail.com>
2024-07-30 12:40:38 +0500

    few mistakes

--- app.js
+++ app.js
@@ -16,24 +16,36 @@ let rigthinp2 = document.getElementById("rigthinp2");
 let para2 = document.getElementById("para2");
 let product = document.getElementById("product");
 let expense = document.getElementById("expense");
-let delBtn=document.getElementById('del-Btn')
-let list=document.getElementById('list')
-list.style.display='none'
+let delBtn = document.getElementById("del-Btn");
+let list = document.getElementById("list");
+let count = 0;
 rightbtn.addEventListener("click", () => {
   para1.innerHTML = rigthinp2.value;
   para2.innerHTML = para.innerHTML - para1.innerHTML;
   if (rigthinp1.value == "") {
     alert("please Enter Your Product 😐");
+return
   }
   if (rigthinp2.value == "") {
     alert("please Enter Your Cost Of Product 🙄");
+    return
   }
   product.innerHTML = rigthinp1.value;
   rigthinp1.value = "";
   expense.innerHTML = rigthinp2.value;
   rigthinp2.value = "";
-  list.style.display='block'
+  if (list.style.display == "none") {
+    list.style.display = "block";
+    list.style.display = "flex";
+    list.style.justifyContent = "space-around";
+  }
+  let items= `<div class="list" id="list" style="display: none;">
+              <h4 id="product"></h4>
+              <h4 id="expense"></h4>
+              <button id="del-Btn"><i class="fa-solid fa-trash-can fa-xl" style="color: #5a7ef1;"></i></button>
+            </div>`;
+list.innerHTML+=items
 });
-delBtn.addEventListener('click',()=>{
-  
-})
+delBtn.addEventListener("click", () => {
+  document.getElementById("list").remove();
+});
