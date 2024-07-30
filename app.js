827884f1a33bbc113cd7462b918f5b9ea1f31e80
muhammadkashif20 <kashif28@gmail.com>
2024-07-30 10:51:22 +0500

    coming soon completed

--- app.js
+++ app.js
@@ -9,16 +9,31 @@ leftbtn.addEventListener("click", () => {
   }
   leftinp.value = "";
 });
-
 let rightbtn = document.getElementById("rightbtn");
 let para1 = document.getElementById("para1");
+let rigthinp1 = document.getElementById("rigthinp1");
 let rigthinp2 = document.getElementById("rigthinp2");
 let para2 = document.getElementById("para2");
+let product = document.getElementById("product");
+let expense = document.getElementById("expense");
+let delBtn=document.getElementById('del-Btn')
+let list=document.getElementById('list')
+list.style.display='none'
 rightbtn.addEventListener("click", () => {
   para1.innerHTML = rigthinp2.value;
   para2.innerHTML = para.innerHTML - para1.innerHTML;
+  if (rigthinp1.value == "") {
+    alert("please Enter Your Product 😐");
+  }
   if (rigthinp2.value == "") {
     alert("please Enter Your Cost Of Product 🙄");
   }
+  product.innerHTML = rigthinp1.value;
+  rigthinp1.value = "";
+  expense.innerHTML = rigthinp2.value;
   rigthinp2.value = "";
+  list.style.display='block'
 });
+delBtn.addEventListener('click',()=>{
+  
+})
